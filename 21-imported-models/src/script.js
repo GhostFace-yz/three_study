38983abb3e18c4dca423bb13eb9279f63184aef6
relic <relic20010420@gmail.com>
2026-07-30 09:56:28 +0800

    模型导入

--- 21-imported-models/src/script.js
+++ 21-imported-models/src/script.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 /**
  * Base
@@ -18,23 +19,30 @@ const scene = new THREE.Scene()
 /**
  * 模型
  */
-const gLTFLoader = new GLTFLoader()
-console.log(gLTFLoader);
+const gltfLoader = new GLTFLoader()
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+gltfLoader.setDRACOLoader(dracoLoader)
 
-gLTFLoader.load(
-  '/models/Duck/glTF/Duck.gltf',
+let mixer = null
+
+gltfLoader.load(
+  '/models/Fox/glTF/Fox.gltf',
   (gltf) => {
-    console.log('成功');
-    console.log(gltf);
-  },
-  (process) => {
-    console.log('加载中');
-    console.log(process);
-  },
-  (err) => {
-    console.log('错误');
-    console.log(err);
-    
+    // scene.add(gltf.scene)
+    // while (gltf.scene.children.length) {
+    //   scene.add(gltf.scene.children[0])
+    // }
+    // const children = [...gltf.scene.children]
+    // for(const child of children) {
+    //   scene.add(child)
+    // }
+    mixer = new THREE.AnimationMixer(gltf.scene)
+    const action = mixer.clipAction(gltf.animations[1])
+    action.play()
+
+    gltf.scene.scale.set(.025, .025, .025)
+    scene.add(gltf.scene)
   }
 )
 
@@ -42,12 +50,12 @@ gLTFLoader.load(
  * Floor
  */
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshStandardMaterial({
-        color: '#444444',
-        metalness: 0,
-        roughness: 0.5
-    })
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: '#444444',
+    metalness: 0,
+    roughness: 0.5
+  })
 )
 floor.receiveShadow = true
 floor.rotation.x = - Math.PI * 0.5
@@ -74,23 +82,22 @@ scene.add(directionalLight)
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -110,7 +117,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
@@ -123,20 +130,25 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock()
 let previousTime = 0
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
 
-    // Update controls
-    controls.update()
+  // 更新动画
+  if (mixer) {
+    mixer.update(deltaTime)
+  }
 
-    // Render
-    renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
