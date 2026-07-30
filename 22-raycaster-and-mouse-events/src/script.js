9b06e83730f56978fcbedf166c6928522cb30294
relic <relic20010420@gmail.com>
2026-07-30 14:25:27 +0800

    射线

---
+++ 22-raycaster-and-mouse-events/src/script.js
@@ -0,0 +1,217 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import GUI from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * 灯光
+ */
+const ambientLight = new THREE.AmbientLight('#ffffff', .7)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1.6)
+directionalLight.position.set(1, 2, 3)
+scene.add(directionalLight)
+
+// 模型加载
+const gltfLoader = new GLTFLoader()
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+gltfLoader.setDRACOLoader(dracoLoader)
+
+let duck = null
+gltfLoader.load(
+  '/models/Duck/glTF-Draco/Duck.gltf',
+  (gltf) => {
+    gltf.scene.position.y = -1.2
+    scene.add(gltf.scene)
+    duck = gltf
+  }
+)
+
+
+/**
+ * Objects
+ */
+const object1 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object1.position.x = - 2
+
+const object2 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+
+const object3 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object3.position.x = 2
+
+scene.add(object1, object2, object3)
+
+/**
+ * 光线投射器raycaster
+ */
+const raycaster = new THREE.Raycaster()
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(10, 0, 0)
+// rayDirection.normalize()
+
+// raycaster.set(rayOrigin, rayDirection)
+
+// const intersect = raycaster.intersectObject(object2)
+// console.log(intersect);
+
+// const intersects = raycaster.intersectObjects([object1, object2, object3])
+// console.log(intersects);
+
+
+
+
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * 鼠标监听
+ */
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (e) => {
+  mouse.x = e.clientX / sizes.width * 2 - 1
+  mouse.y = -(e.clientY / sizes.height) * 2 + 1
+})
+
+window.addEventListener('click', () => {
+  if (currentIntersect) {
+    switch (currentIntersect.object) {
+      case object1:
+        console.log('点击球1');
+        break;
+      case object2:
+        console.log('点击球2');
+        break;
+      case object3:
+        console.log('点击球3');
+        break;
+    }
+
+  }
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(10, 0, 0)
+// rayDirection.normalize()
+// raycaster.set(rayOrigin, rayDirection)
+
+let currentIntersect = null
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // 动画
+  object1.position.y = Math.sin(elapsedTime * .3) * 1.5
+  object2.position.y = Math.sin(elapsedTime * .8) * 1.5
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+
+  raycaster.setFromCamera(mouse, camera)
+  object1.material.color.set("#ff0000")
+  object2.material.color.set("#ff0000")
+  object3.material.color.set("#ff0000")
+
+  const intersects = raycaster.intersectObjects([object1, object2, object3])
+  for (const intersect of intersects) {
+    intersect.object.material.color.set('#0000ff')
+  }
+
+  if (intersects.length) {
+    if (!currentIntersect) {
+      console.log('鼠标进入');
+    }
+    currentIntersect = intersects[0]
+  } else {
+    currentIntersect = null
+  }
+  if (duck) {
+    const modelIntersects = raycaster.intersectObject(duck.scene)
+    if(modelIntersects.length > 0) {
+      duck.scene.scale.set(1.2, 1.2, 1.2)
+    } else {
+      duck.scene.scale.set(1, 1, 1)
+    }
+  }
+
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
